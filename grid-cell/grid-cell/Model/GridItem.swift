e403c1561927099d7e8296ab3c0d85cba79db3e6
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-22 16:36:45 +0800

    [feat] Add `GridItem`

---
+++ grid-cell/grid-cell/Model/GridItem.swift
@@ -0,0 +1,14 @@
+//
+//  GridItem.swift
+//  grid-cell
+//
+//  Created by Yi-Chin Hsu on 2024/2/22.
+//
+
+import Foundation
+
+struct GridItem {
+  let id: String
+  let title: String
+  let thumbnailUrl: String
+}
