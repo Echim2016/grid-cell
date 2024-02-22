8196f45f88ff313dbfe7e0098b91e50e9c961e8d
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-22 18:54:19 +0800

    [feat] Add `AlamofireHTTPClient`

---
+++ grid-cell/grid-cell/Networking/AlamofireHTTPClient.swift
@@ -0,0 +1,22 @@
+//
+//  AlamofireHTTPClient.swift
+//  grid-cell
+//
+//  Created by Yi-Chin Hsu on 2024/2/22.
+//
+
+import Alamofire
+import Foundation
+
+final class AlamofireHTTPClient: HTTPClient {
+  func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+    AF.request(url)
+      .response { response in
+        if let data = response.data {
+          completion(.success(data))
+        } else if let error = response.error {
+          completion(.failure(error))
+        }
+      }
+  }
+}
