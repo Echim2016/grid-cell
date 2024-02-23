83c94ecc778d87abcc5182e6d2c6c08bb36213b1
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-23 14:24:23 +0800

    [feat] Add `LocalGridImageDataLoader`

---
+++ grid-cell/grid-cell/Cache/LocalGridImageDataLoader.swift
@@ -0,0 +1,16 @@
+//
+//  LocalGridImageDataLoader.swift
+//  grid-cell
+//
+//  Created by Yi-Chin Hsu on 2024/2/23.
+//
+
+import Foundation
+
+final class LocalGridImageDataLoader {
+  let store: GridImageDataStore
+  
+  init(store: GridImageDataStore) {
+    self.store = store
+  }
+}
