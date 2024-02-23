bab64ce143f1f8d71b09c616c8c4ba1ea5aab9fe
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-23 18:17:39 +0800

    [refactor] Move `GridItemCellRenderController` to single file

---
+++ grid-cell/grid-cell/View/Cell/GridItemCellRenderController.swift
@@ -0,0 +1,38 @@
+//
+//  GridItemCellRenderController.swift
+//  grid-cell
+//
+//  Created by Yi-Chin Hsu on 2024/2/23.
+//
+
+import Foundation
+
+final class GridItemCellRenderController {
+  private var cell: GridItemCell? = nil
+  private let viewModel: GridItemCellViewModel
+  
+  init(viewModel: GridItemCellViewModel) {
+    self.viewModel = viewModel
+  }
+  
+  func setupCell(with cell: GridItemCell) {
+    self.cell = cell
+  }
+  
+  func preload() {
+    viewModel.loadImage { [weak self] result in
+      switch result {
+      case let .success(data):
+        self?.cell?.updateImage(with: data)
+      case .failure:
+        break
+      }
+    }
+    cell?.updateUI(with: viewModel)
+  }
+  
+  func cancelLoad() {
+    viewModel.cancelLoadImageTask()
+    cell = nil
+  }
+}
--- grid-cell/grid-cell/View/ViewController/GridViewController.swift
+++ grid-cell/grid-cell/View/ViewController/GridViewController.swift
@@ -8,36 +8,6 @@
 import RxSwift
 import UIKit
 
-final class GridItemCellRenderController {
-  private var cell: GridItemCell? = nil
-  private let viewModel: GridItemCellViewModel
-  
-  init(viewModel: GridItemCellViewModel) {
-    self.viewModel = viewModel
-  }
-  
-  func setupCell(with cell: GridItemCell) {
-    self.cell = cell
-  }
-  
-  func preload() {
-    viewModel.loadImage { [weak self] result in
-      switch result {
-      case let .success(data):
-        self?.cell?.updateImage(with: data)
-      case .failure:
-        break
-      }
-    }
-    cell?.updateUI(with: viewModel)
-  }
-  
-  func cancelLoad() {
-    viewModel.cancelLoadImageTask()
-    cell = nil
-  }
-}
-
 final class GridViewModel {
   let gridLoader: GridLoader
   let imageLoader: GridImageDataLoader
