59997f578be5ab19351b689b84211f03df9cc328
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-23 15:01:04 +0800

    [refactor] Move `GridImageFileStore` to single file

--- grid-cell/grid-cell/Cache/GridImageDataStore.swift
+++ grid-cell/grid-cell/Cache/GridImageDataStore.swift
@@ -15,23 +15,3 @@ protocol GridImageDataStore {
 enum FileStore {
   static let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
 }
-
-class GridImageFileStore: GridImageDataStore {
-  let storeURL: URL
-  
-  init(storeURL: URL = FileStore.storeURL) {
-    self.storeURL = storeURL
-  }
-  
-  func insert(_ data: Data, for url: URL) throws {
-    try data.write(to: composeStorePath(with: url))
-  }
-  
-  func retrieve(from url: URL) throws -> Data? {
-    try Data(contentsOf: composeStorePath(with: url))
-  }
-  
-  private func composeStorePath(with url: URL) -> URL {
-    storeURL.appendingPathComponent(url.lastPathComponent)
-  }
-}
---
+++ grid-cell/grid-cell/Cache/GridImageFileStore.swift
@@ -0,0 +1,28 @@
+//
+//  GridImageFileStore.swift
+//  grid-cell
+//
+//  Created by Yi-Chin Hsu on 2024/2/23.
+//
+
+import Foundation
+
+final class GridImageFileStore: GridImageDataStore {
+  let storeURL: URL
+  
+  init(storeURL: URL = FileStore.storeURL) {
+    self.storeURL = storeURL
+  }
+  
+  func insert(_ data: Data, for url: URL) throws {
+    try data.write(to: composeStorePath(with: url))
+  }
+  
+  func retrieve(from url: URL) throws -> Data? {
+    try Data(contentsOf: composeStorePath(with: url))
+  }
+  
+  private func composeStorePath(with url: URL) -> URL {
+    storeURL.appendingPathComponent(url.lastPathComponent)
+  }
+}
