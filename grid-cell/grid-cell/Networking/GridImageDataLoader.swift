69d62968d43ae76fadda982c3f93c99afe429a21
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-23 14:17:43 +0800

    [feat] Add `GridImageDataLoader`

---
+++ grid-cell/grid-cell/Networking/GridImageDataLoader.swift
@@ -0,0 +1,14 @@
+//
+//  GridImageDataLoader.swift
+//  grid-cell
+//
+//  Created by Yi-Chin Hsu on 2024/2/23.
+//
+
+import Foundation
+
+protocol GridImageDataLoader {
+  typealias Result = Swift.Result<(Data), Error>
+
+  func loadImageData(from url: URL, completion: @escaping (Result) -> Void)
+}
