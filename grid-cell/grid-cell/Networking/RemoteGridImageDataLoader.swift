bd1357d5a29e366d12f35081fcb9faf32fc8de1a
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-23 14:42:04 +0800

    [feat] Add `RemoteGridImageDataLoaderWithCache`

--- grid-cell/grid-cell/Networking/RemoteGridImageDataLoader.swift
+++ grid-cell/grid-cell/Networking/RemoteGridImageDataLoader.swift
@@ -18,3 +18,29 @@ final class RemoteGridImageDataLoader: GridImageDataLoader {
     client.get(from: url, completion: completion)
   }
 }
+
+final class RemoteGridImageDataLoaderWithCache: GridImageDataLoader {
+  let remoteLoader: GridImageDataLoader
+  let cache: GridImageDataCache
+  
+  init(remoteLoader: GridImageDataLoader, cache: GridImageDataCache) {
+    self.remoteLoader = remoteLoader
+    self.cache = cache
+  }
+  
+  func loadImageData(from url: URL, completion: @escaping (GridImageDataLoader.Result) -> Void) {
+    remoteLoader.loadImageData(from: url) { [weak self] result in
+      switch result {
+      case let .success(data):
+        completion(.success(data))
+        do {
+          try self?.cache.save(data, for: url)
+        } catch {
+          completion(.failure(error))
+        }
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    }
+  }
+}
