cbf6c869a3cff65462b78e6a74460e8e6ac31621
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-23 14:26:00 +0800

    [feat] Apply image loading method to `LocalGridImageDataLoader`

--- grid-cell/grid-cell/Cache/LocalGridImageDataLoader.swift
+++ grid-cell/grid-cell/Cache/LocalGridImageDataLoader.swift
@@ -9,8 +9,26 @@ import Foundation
 
 final class LocalGridImageDataLoader {
   let store: GridImageDataStore
-  
+
   init(store: GridImageDataStore) {
     self.store = store
+  }
+}
+
+extension LocalGridImageDataLoader: GridImageDataLoader {
+  enum LocalLoadError: Error {
+    case failed
+    case notFound
+  }
+
+  func loadImageData(from url: URL, completion: @escaping (GridImageDataLoader.Result) -> Void) {
+    do {
+      if let imageData = try store.retrieve(from: url) {
+        completion(.success(imageData))
+      }
+    } catch {
+      completion(.failure(LocalLoadError.failed))
+    }
+    completion(.failure(LocalLoadError.notFound))
   }
 }
