7e74de83565257738527758a7ca3c3b9a1091c1c
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-23 14:21:10 +0800

    [feat] Add `GridImageFileStore` for caching image data

--- grid-cell/grid-cell/Cache/GridImageDataStore.swift
+++ grid-cell/grid-cell/Cache/GridImageDataStore.swift
@@ -11,3 +11,27 @@ protocol GridImageDataStore {
   func insert(_ data: Data, for url: URL) throws
   func retrieve(from url: URL) throws -> Data?
 }
+
+enum FileStore {
+  static let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+}
+
+class GridImageFileStore: GridImageDataStore {
+  let storeURL: URL
+  
+  init(storeURL: URL = FileStore.storeURL) {
+    self.storeURL = storeURL
+  }
+  
+  func insert(_ data: Data, for url: URL) throws {
+    try data.write(to: composeStorePath(with: url))
+  }
+  
+  func retrieve(from url: URL) throws -> Data? {
+    try Data(contentsOf: composeStorePath(with: url))
+  }
+  
+  private func composeStorePath(with url: URL) -> URL {
+    storeURL.appendingPathComponent(url.lastPathComponent)
+  }
+}
