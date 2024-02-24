54964f4aab153f6beb83998dd71e00b49d01df67
Yi-Chin Hsu (Echim) <ychim26@gmail.com>
2024-02-24 10:49:46 +0800

    [feat] Add `GridCellHomePageTests`

---
+++ grid-cell/grid-cellTests/GridCellHomePageTests.swift
@@ -0,0 +1,23 @@
+//
+//  GridCellHomePageTests.swift
+//  grid-cellTests
+//
+//  Created by Yi-Chin Hsu on 2024/2/22.
+//
+
+import XCTest
+@testable import grid_cell
+
+final class GridCellHomePageTests: XCTestCase {
+
+  func test_homePage_navigationBarTitle() {
+    let viewModel = HomeViewModel()
+    let homeViewController = HomeViewController(
+      viewModel: viewModel,
+      coordinator: nil
+    )
+    homeViewController.viewDidLoad()
+    
+    XCTAssertEqual(homeViewController.title, viewModel.navigationBarTitle)
+  }
+}
